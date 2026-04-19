726c5815418a503239a03375ec0cd19d6b0a94b9
alshamqueg <104331611+alshamqueg@users.noreply.github.com>
2026-04-19 15:46:20 -0400

    queries.sql

--- queries.sql
+++ queries.sql
@@ -9,3 +9,9 @@ SELECT p.ProductName, SUM(f.QuantitySold) AS TotalSold
 FROM DWDAYSALESFACT f
 JOIN DWPRODUCT p ON f.ProductKey = p.ProductKey
 GROUP BY p.ProductName;
+
+-- Total Profit by Product
+SELECT p.ProductName, SUM(f.Profit) AS TotalProfit
+FROM DWDAYSALESFACT f
+JOIN DWPRODUCT p ON f.ProductKey = p.ProductKey
+GROUP BY p.ProductName;
