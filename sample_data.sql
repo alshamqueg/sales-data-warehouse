80f658bb2477c2afd60388ce81fa3167ca95d966
alshamqueg <104331611+alshamqueg@users.noreply.github.com>
2026-04-19 15:40:37 -0400

    sample_data.sql

--- sample_data.sql
+++ sample_data.sql
@@ -10,7 +10,8 @@ INSERT INTO DWPRODUCT VALUES (2, 'Phone', 'Electronics');
 INSERT INTO DWREGION VALUES (1, 'Central', 'FL', 'South');
 
 -- Time
-INSERT INTO DWTIME VALUES (1, '2026-01-01', 2026, 1, 'January');
+INSERT INTO DWTIME (TimeKey, FullDate, Year, Month, MonthName)
+VALUES (1, '2026-01-01', 2026, 1, 'January');
 
 -- Vendor
 INSERT INTO DWVENDOR VALUES (1, 'TechSupplier', 'USA', 'FL');
